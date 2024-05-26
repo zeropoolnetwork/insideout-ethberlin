5862b2e2479ee33d183d980e282177bb5504bef0
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-05-26 11:58:59 +0500

    implement hashing based on Noir code

--- storage-node/src/mining.ts
+++ storage-node/src/mining.ts
@@ -2,31 +2,64 @@ import { Fr } from '@aztec/bb.js';
 import { SparseMerkleTree } from './sparse-merkle-tree';
 import seedrandom from 'seedrandom';
 
-// FIXME: WIP
-export async function mine(tree: SparseMerkleTree, oracle: Fr, numSamples: number, nonce: number, difficulty: bigint): Promise<bigint> {
-  const bb = tree.bb;
+const ACCOUNT_AND_DATA_TREE_DEPTH = 10;
+const FILE_TREE_DEPTH = 10;
 
-  let hash = 0n;
-  do {
-    const samples: Fr[] = await sampleLeaves(tree, numSamples, nonce);
-    const h = await bb.poseidon2Hash([...samples, oracle]);
-    hash = BigInt(h.toString());
-  } while (hash > difficulty);
+type MiningResult = {
+  mining_nonce: number,
+  bruteforce_hash: Fr,
+  index_hash: Fr,
+  index: number,
+  data: Fr,
+  mining_hash: Fr,
+};
 
-  return hash;
-}
+function trim(f: Fr, n: number): Fr {
+  const fBigInt: bigint = BigInt(f.toString());
+  let res = 0n;
+  let cur = 1n;
 
-async function sampleLeaves(tree: SparseMerkleTree, numSamples: number, seed: number): Promise<Fr[]> {
-  const rng = seedrandom(seed);
-  const samples = [];
-  const numLeaves = tree.capacity();
-  const indicesTaken = [];
-
-  for (let i = 0; i < numSamples; i++) {
-    let index = Math.floor(rng() * numLeaves);
-    samples.push(await tree.getLeaf(index));
-    indicesTaken.push(index);
+  for (let i = 0; i < n; i++) {
+    const bit = fBigInt & cur;
+    res = res | bit;
   }
 
-  return samples;
+  return Fr.fromString(res.toString());
+}
+
+// FIXME: we use one one sameple, production will use more
+//
+//
+export async function mine(
+  tree: SparseMerkleTree,
+  pk: Fr,
+  oracle: Fr,
+  prg_seed: number,
+): Promise<MiningResult | null> {
+  const bb = tree.bb;
+  const MAX_MINING_NONCE = 1048576;
+  const DIFFICULTY = 28269553036454149273332760011886696253239742350009903329945699220681916416n;
+
+  for (let mining_nonce = 0; mining_nonce < MAX_MINING_NONCE; mining_nonce++) {
+    const bruteforce_hash =
+      await bb.poseidon2Hash([pk, oracle, Fr.fromString(mining_nonce.toString())]);
+    const index_hash = 
+      await bb.poseidon2Hash([bruteforce_hash]);
+    const index : number = Number(trim(index_hash, ACCOUNT_AND_DATA_TREE_DEPTH + FILE_TREE_DEPTH).toString());
+    const data = await tree.getLeaf(index);
+    const mining_hash =
+      await bb.poseidon2Hash([bruteforce_hash, data]);
+
+    if (BigInt(mining_hash.toString()) < DIFFICULTY)
+      return {
+        mining_nonce: mining_nonce,
+        bruteforce_hash: bruteforce_hash,
+        index_hash: index_hash,
+        index: index,
+        data: data,
+        mining_hash: mining_hash,
+      }
+  }
+
+  return null;
 }
