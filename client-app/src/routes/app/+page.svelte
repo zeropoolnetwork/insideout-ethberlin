32309be652be88426ef4e21616337ab40014e73c
Dmitry Vdovin <voidxnull@gmail.com>
2024-05-26 11:43:05 +0400

    Fix margin

--- client-app/src/routes/app/+page.svelte
+++ client-app/src/routes/app/+page.svelte
@@ -111,7 +111,7 @@
 			<h2 class="text-xl font-bold mb-2">Files</h2>
 			<button
 				on:click={handleUpload}
-				class="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded"
+				class="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mb-2"
 			>
 				Upload
 			</button>
